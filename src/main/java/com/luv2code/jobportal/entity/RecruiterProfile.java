be8a4a6b68ae209feffac6cb3e5249c54e621e28
Fouche du Preez <fouche@limehouse.co.za>
2024-12-01 22:25:02 +0200

    Added profiles (wip)

---
+++ src/main/java/com/luv2code/jobportal/entity/RecruiterProfile.java
@@ -0,0 +1,132 @@
+package com.luv2code.jobportal.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "recruiter_profile")
+public class RecruiterProfile {
+
+    @Id
+    private int userAccountId;
+
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    private Users userId;
+
+    private String firstName;
+    private String lasName;
+    private String city;
+    private String state;
+    private String country;
+    private String company;
+
+    @Column(nullable = true, length = 64)
+    private String profilePhoto;
+
+    public RecruiterProfile() {
+    }
+
+    public RecruiterProfile(Users userId) {
+        this.userId = userId;
+    }
+
+    public RecruiterProfile(int userAccountId, Users userId, String firstName, String lasName,
+                            String city, String state, String country, String company, String profilePhoto) {
+        this.userAccountId = userAccountId;
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lasName = lasName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.company = company;
+        this.profilePhoto = profilePhoto;
+    }
+
+    public int getUserAccountId() {
+        return userAccountId;
+    }
+
+    public void setUserAccountId(int userAccountId) {
+        this.userAccountId = userAccountId;
+    }
+
+    public Users getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Users userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasName() {
+        return lasName;
+    }
+
+    public void setLasName(String lasName) {
+        this.lasName = lasName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    @Override
+    public String toString() {
+        return "RecruiterProfile{" +
+                "userAccountId=" + userAccountId +
+                ", userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lasName='" + lasName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", company='" + company + '\'' +
+                ", profilePhoto='" + profilePhoto + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/luv2code/jobportal/services/UsersService.java
+++ src/main/java/com/luv2code/jobportal/services/UsersService.java
@@ -1,6 +1,10 @@
 package com.luv2code.jobportal.services;
 
+import com.luv2code.jobportal.entity.JobSeekerProfile;
+import com.luv2code.jobportal.entity.RecruiterProfile;
 import com.luv2code.jobportal.entity.Users;
+import com.luv2code.jobportal.repository.JobSeekerProfileRepository;
+import com.luv2code.jobportal.repository.RecruiterProfileRepository;
 import com.luv2code.jobportal.repository.UsersRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +15,28 @@ import java.util.Optional;
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+    private final RecruiterProfileRepository recruiterProfileRepository;
 
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository, JobSeekerProfileRepository jobSeekerProfileRepository, RecruiterProfileRepository recruiterProfileRepository) {
         this.usersRepository = usersRepository;
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+        this.recruiterProfileRepository = recruiterProfileRepository;
     }
 
     public Users addNew(Users user) {
         user.setActive(true);
         user.setRegistrationDate(new Date(System.currentTimeMillis()));
-        return usersRepository.save(user);
+        user = usersRepository.save(user);
+
+        int userTypeId = user.getUserTypeId().getUserTypeId();
+        if (userTypeId == 1) {
+            recruiterProfileRepository.save(new RecruiterProfile(user));
+        } else {
+            jobSeekerProfileRepository.save(new JobSeekerProfile(user));
+        }
+
+        return user;
     }
 
     public Optional<Users> getUserByEmail(String email) {
