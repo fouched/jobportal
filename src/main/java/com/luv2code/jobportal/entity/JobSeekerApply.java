64eeb427d95bf32ade0ae55e3300dca1f357d3b9
Fouche du Preez <fouche@limehouse.co.za>
2024-12-21 20:47:32 +0200

    added entities for job applications

---
+++ src/main/java/com/luv2code/jobportal/entity/JobSeekerApply.java
@@ -0,0 +1,95 @@
+package com.luv2code.jobportal.entity;
+
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "job_seeker_apply",
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"user_id", "job"})
+        }
+)
+public class JobSeekerApply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // Below - No need for referencedColumnName - the default joins to the primary key
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private JobSeekerProfile userId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job")
+    private JobPostActivity job;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date applyDate;
+
+    private String coverLetter;
+
+    public JobSeekerApply() {
+    }
+
+    public JobSeekerApply(Integer id, JobSeekerProfile userId, JobPostActivity job, Date applyDate, String coverLetter) {
+        this.id = id;
+        this.userId = userId;
+        this.job = job;
+        this.applyDate = applyDate;
+        this.coverLetter = coverLetter;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public JobSeekerProfile getUserId() {
+        return userId;
+    }
+
+    public void setUserId(JobSeekerProfile userId) {
+        this.userId = userId;
+    }
+
+    public JobPostActivity getJob() {
+        return job;
+    }
+
+    public void setJob(JobPostActivity job) {
+        this.job = job;
+    }
+
+    public Date getApplyDate() {
+        return applyDate;
+    }
+
+    public void setApplyDate(Date applyDate) {
+        this.applyDate = applyDate;
+    }
+
+    public String getCoverLetter() {
+        return coverLetter;
+    }
+
+    public void setCoverLetter(String coverLetter) {
+        this.coverLetter = coverLetter;
+    }
+
+    @Override
+    public String toString() {
+        return "JobSeekerApply{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", job=" + job +
+                ", applyDate=" + applyDate +
+                ", coverLetter='" + coverLetter + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/luv2code/jobportal/entity/JobSeekerSave.java
@@ -0,0 +1,67 @@
+package com.luv2code.jobportal.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "job_seeker_save",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "job"})
+        }
+)
+public class JobSeekerSave {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // Below - No need for referencedColumnName - the default joins to the primary key
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private JobSeekerProfile userId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "job")
+    private JobPostActivity job;
+
+    public JobSeekerSave() {
+    }
+
+    public JobSeekerSave(Integer id, JobSeekerProfile userId, JobPostActivity job) {
+        this.id = id;
+        this.userId = userId;
+        this.job = job;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public JobSeekerProfile getUserId() {
+        return userId;
+    }
+
+    public void setUserId(JobSeekerProfile userId) {
+        this.userId = userId;
+    }
+
+    public JobPostActivity getJob() {
+        return job;
+    }
+
+    public void setJob(JobPostActivity job) {
+        this.job = job;
+    }
+
+    @Override
+    public String toString() {
+        return "JobSeekerSave{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", job=" + job +
+                '}';
+    }
+}
